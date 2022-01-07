4e2b6a6167c510710cd3a5c6e27e84cd80112ff4
djgamertri <djgamertri@gmail.com>
2022-01-07 08:28:21 -0500

    (V3)

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const sec1 = document.getElementById("sec1");
 const sec2 = document.getElementById("sec2");
 const sec3 = document.getElementById("sec3");
 const sec4 = document.getElementById("sec4");
+const sec5 = document.getElementById("sec5");
 
 const LoadImage = (entradas, observador) => { 
 
@@ -31,6 +32,7 @@ observador.observe(sec1);
 observador.observe(sec2);
 observador.observe(sec3);
 observador.observe(sec4);
+observador.observe(sec5);
 
 //Loader
 
