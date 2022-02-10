d8dcb7e84865549dd7caba355a7cf886bf5390e5
djgamertri <djgamertri@gmail.com>
2022-02-09 22:21:26 -0500

    Correcion

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 //observer
 
+const secincio = document.getElementById("sec-incio");
 const sec1 = document.getElementById("sec1");
 const sec2 = document.getElementById("sec2");
 const sec3 = document.getElementById("sec3");
 const sec4 = document.getElementById("sec4");
 const sec5 = document.getElementById("sec5");
+const sec6 = document.getElementById("sec6");
 
 const LoadImage = (entradas, observador) => { 
 
@@ -28,11 +30,13 @@ const observador = new IntersectionObserver(LoadImage, {
     threshold: 1.0
 });
 
+observador.observe(secincio);
 observador.observe(sec1);
 observador.observe(sec2);
 observador.observe(sec3);
 observador.observe(sec4);
 observador.observe(sec5);
+observador.observe(sec6);
 
 //Loader
 
