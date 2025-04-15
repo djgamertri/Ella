80e4a5fe1c3e9dc5ea916fa941eda45a35715fb3
djgamertri_ <djgamertri@gmail.com>
2025-04-14 23:54:53 -0500

    Astro deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: 'https://djgamertri.github.io',
+  base: 'Ella'
 })
